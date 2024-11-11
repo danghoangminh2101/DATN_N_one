98e23089796fe906a7aebe08f813f8c8382812ad
Pham Duy <duyp7454@gmail.com>
2024-11-11 23:17:27 +0700

    update layout

--- src/app/listProduct/component/ProductGrid.tsx
+++ src/app/listProduct/component/ProductGrid.tsx
@@ -95,13 +95,16 @@ function ProductGrid() {
         },
       ];
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 lg:gap-6'>
+    <div className='flex justify-center'>
+    <div className="grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-[1rem]">
       
-    {products.map((product) => (
-     <ProductItem product={product} key={product.id}/>
-    ))} 
-  
-      </div>
+      {products.map((product) => (
+       <ProductItem product={product} key={product.id}/>
+      ))} 
+    
+        </div>
+    </div>
+ 
   )
 }
 
--- src/app/listProduct/page.tsx
+++ src/app/listProduct/page.tsx
@@ -9,10 +9,10 @@ function ListProduct() {
     const [grid, setGrid] = useState(true); // true for grid view, false for list view
 
     return (
-        <div className="w-full min-h-screen bg-gray-50 px-4 py-6 md:px-8 lg:px-0 lg:w-[90%] xl:w-[80%] mx-auto">
+        <div className="w-full min-h-screen px-4 py-6 md:px-8 lg:px-0 lg:w-[90%] xl:w-[80%] mx-auto">
             <div className="flex flex-col lg:flex-row gap-6">
                 {/* Sidebar */}
-                <div className="w-full lg:w-[20rem] h-[30rem] bg-white rounded-xl  shadow-md p-4">
+                <div className="w-full lg:w-[15rem] 2xl:w-[20rem] h-[30rem] bg-white rounded-xl  shadow-md p-4">
                     <h4 className="text-black font-semibold text-lg pb-2 border-b border-gray-200">Danh Mục</h4>
                     <ul className="text-sm">
                         {['Điện thoại', 'Laptop', 'Tablet', 'Tai nghe'].map((item, index) => (
@@ -39,7 +39,7 @@ function ListProduct() {
                 {/* Product area */}
                 <div className="flex-1">
                     {/* Toggle View */}
-                    <div className="flex gap-4 text-xl items-center justify-end mb-6">
+                    <div className="flex gap-4 text-xl items-center justify-center mb-6">
                         <div 
                             className={`border border-gray-300 rounded-lg p-2.5 cursor-pointer transition-all ${grid ? 'bg-blue-500 text-white' : 'hover:bg-gray-100'}`}
                             onClick={() => setGrid(true)}
